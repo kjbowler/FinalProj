74b9163397661888aa1bcdd9b28d4276c6b89513
Kayla Bowler <kbowler@ucsc.edu>
2020-03-04 17:33:12 -0800

    graphs

--- student-alcohol-consumption/simple-graphs.py
+++ student-alcohol-consumption/simple-graphs.py
@@ -2,17 +2,39 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-combData = pd.read_csv("combData.csv")
-unique = combData.Dalc_x.unique()
+combData = pd.read_csv("combData-normal.csv")
+dataX = combData.drop(columns = ['guardian_y', 'traveltime_y', 'studytime_y', 'failures_y', 'schoolsup_y', 'famsup_y', 'paid_y', 'activities_y', 'higher_y', 'freetime_y', 'goout_y', 'Dalc_y', 'Walc_y', 'health_y', 'absences_y', 'G1_y', 'G2_y', 'G3_y'])
+dataY = combData.drop(columns = ['guardian_x', 'traveltime_x', 'studytime_x', 'failures_x', 'schoolsup_x', 'famsup_x', 'paid_x', 'activities_x', 'higher_x', 'freetime_x', 'goout_y', 'Dalc_x', 'Walc_x', 'health_x', 'absences_x', 'G1_x', 'G2_x', 'G3_x'])
+#print(combData.shape, dataX.shape, dataY.shape)
+uniqueX = [0,1]
+uniqueY = [0,1]
 findMax = []
-for x in unique:
-    newdf = combData[combData["Dalc_x"] == x]
-    avgx = newdf["G3_x"].mean()
-    findMax.append(newdf.shape[0])
-    #print(newdf)
-    plt.plot(x, avgx, marker = "x", color = 'm')
-# combData.plot.scatter(x = "Dalc_y", y = "G3_y", marker = ".", color = 'blue')
-print(findMax)
+avgX = []
+avgY = []
+trac = []
+for x in uniqueX:
+    #newdfX = dataX[dataX["Dalc_x"] == x]
+    #newdfX = dataX[dataX["failures_x"] == x]
+    #newdfX = dataX[dataX["studytime_x"] == x]
+    #newdfX = dataX[dataX["absences_x"] == x]
+    newdfX = dataX[dataX["paid_x"] == x]
+    #newdfX = dataX[dataX["internet"] == x]
+    avgX.append(newdfX["G3_x"].mean())
+    trac.append(x)
+    #newdfY= dataY[dataY["Dalc_y"] == x]
+    #newdfY = dataY[dataY["failures_y"] == x]
+    #newdfY = dataY[dataY["studytime_y"] == x]
+    #newdfY = dataY[dataY["absences_y"] == x]
+    newdfY = dataY[dataY["paid_y"] == x]
+    #newdfY = dataY[dataY["internet"] == x]
+    avgY.append(newdfY["G3_y"].mean())
 
+x_label = 'Extra Paid Classes'
+y_label = 'Final Grade'
+plt.xlabel(x_label)
+plt.ylabel(y_label)
+plt.plot(trac, avgX, marker = "x", color = 'm', label = "Math Class")
+plt.plot(trac, avgY, marker = "x", color = 'c', label = "Portuguese Class")
 
+plt.legend()
 plt.show()
